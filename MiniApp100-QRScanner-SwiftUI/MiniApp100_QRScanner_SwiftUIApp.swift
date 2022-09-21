1eb5dec05b80045f94a7f324f4476bc3eb5ee03d
Kota <maedakoutaa@gmail.com>
2022-09-21 09:49:00 +0900

    Initial Commit

---
+++ MiniApp100-QRScanner-SwiftUI/MiniApp100_QRScanner_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp100_QRScanner_SwiftUIApp.swift
+//  MiniApp100-QRScanner-SwiftUI
+//
+//  Created by 前田航汰 on 2022/09/21.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp100_QRScanner_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
